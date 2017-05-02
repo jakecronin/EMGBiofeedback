e9b02695752483e4fcfbb344d39945257653dcab
jakecronin <jake.cronin@emory.edu>
2017-05-02 16:23:48 -0400

    reads and modifies text based on file.

---
+++ StationaryVisualFeedbackVR/assets/updateText.cs
@@ -0,0 +1,39 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEditor;
+using UnityEngine.UI;
+
+public class updateText : MonoBehaviour {
+
+	public Text textAsset;
+	public string path;
+
+	// Use this for initialization
+	void Start () {
+		textAsset = GetComponent<Text>();
+		if (path == null) {
+			path = "AssetsItem/JakeData.txt";
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		writeToString(ReadString());
+	}
+
+	string ReadString()
+	{
+		//Read the text from directly from the test.txt file
+		StreamReader reader = new StreamReader(path); 
+		string data = reader.ReadToEnd ();
+		Debug.Log(data);
+		reader.Close();
+		return data;
+	}
+	void writeToString(string toAppend){
+		textAsset.text = "Percent force: " + toAppend;
+	}
+}
