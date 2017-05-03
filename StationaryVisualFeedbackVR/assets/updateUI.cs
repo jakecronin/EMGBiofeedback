4b367954e88afa0845b5b6329dd1f11f466084d8
Jake Cronin <jake.cronin4@gmail.com>
2017-05-03 11:15:58 -0400

    re-scales on click

--- StationaryVisualFeedbackVR/assets/updateUI.cs
+++ StationaryVisualFeedbackVR/assets/updateUI.cs
@@ -10,16 +10,47 @@ public class updateUI : MonoBehaviour {
 	public float min;
 	public float max;
 	public GameObject textObject;
-	private Text textToModify;
 	public string path;
 	public RectTransform transform;
 
+	public GameObject controllerLeft;
+
+	//used for haptic feedback
+	private SteamVR_TrackedObject trackedObject;
+	private SteamVR_Controller.Device device;
+
+	//Used for modifying text
+	private Text textToModify;
+
+	//used for delegate
+	private SteamVR_TrackedController controller;
+
 	// Use this for initialization
 	void Start () {
+
+		//load objects
 		textToModify = textObject.GetComponent<Text> ();
 		transform = GetComponent<RectTransform> ();
+		controller = controllerLeft.GetComponent<SteamVR_TrackedController> ();
+		controller.TriggerClicked += TriggerPressed;
+		trackedObject = controllerLeft.GetComponent<SteamVR_TrackedObject> ();
 	}
-	
+
+	void TriggerPressed(object sender, ClickedEventArgs e){
+		updateMax ();
+
+		device = SteamVR_Controller.Input((int)trackedObject.index);
+		device.TriggerHapticPulse (900);
+	}
+
+	void updateMax(){
+		StreamReader sr = new StreamReader (path);
+		string data = sr.ReadToEnd ();
+		sr.Close ();
+		float value = (float)double.Parse (data);
+		max = value;
+	}
+
 	// Update is called once per frame
 	void Update () {
 		//read data
@@ -30,9 +61,11 @@ public class updateUI : MonoBehaviour {
 		textToModify.text = "Percent Force: " + data;
 
 		//convert to double and send to slideView
-		double value = double.Parse (data);
-		Debug.Log (data);
-
+		float value = (float) double.Parse (data);
+		if (value > max) {
+			value = max;
+			Debug.Log ("updated max");
+		}
 		//scale data
 		value = ((value / (max - min)) + min) * 100;
 
