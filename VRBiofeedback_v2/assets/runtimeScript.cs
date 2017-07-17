ebfeb0ed9eb70801f0304dde58f36f63094eaf52
Jake Cronin <jake.cronin4@gmail.com>
2017-07-17 11:28:06 -0400

    VR scaled to percentages

--- VRBiofeedback_v2/assets/runtimeScript.cs
+++ VRBiofeedback_v2/assets/runtimeScript.cs
@@ -13,20 +13,23 @@ public class runtimeScript : MonoBehaviour {
 	public Image targetObject;		//target to hit
 	public Image dotObject;			//moving dot
 
-	public GameObject leftController;
-	public GameObject rightController;
+	public GameObject leftController;	//VR Controller
+	public GameObject rightController;	//VR Controller
 
-	public float leftMax;
-	public float rightMax;
+	//public float leftMax; unnecessary, scaled in labview
+	//public float rightMax; unnecessary, scaled in labview
 
 	public float dotHeight;
 	public float dotWidth;
 
-	public float targetHeight;
-	public float targetWidth;
+	//public float targetHeight; unnecessary, read from labview
+	//public float targetWidth;	 unnecessary, read from labview
 
 	public string pathToRightData;	//filepath to emg data for right muscles
-	public string pathToLeftData;	//filepath to emg data for left muscles
+	public string pathToLeftData;   //filepath to emg data for left muscles
+
+	public string pathToLRErrorData;
+	public string pathToAmplitudeErrorData;
 
 	public string pathToTargetPercentage;	//filepath to number that represents target activation
 
@@ -54,21 +57,21 @@ public class runtimeScript : MonoBehaviour {
 		float rightValue = getRightData();
 		float leftValue = getLeftData();
 
-		//Calculate left and right values as percentage of their max
-		if (rightValue>rightMax) {
-			rightMax = rightValue;
-		}
-		float rightPercent = rightValue / rightMax;
+		//Don't need to scale anymore because values are percentages
+		//if (rightValue>rightMax) {
+		//	rightMax = rightValue;
+		//}
+		//float rightPercent = rightValue / rightMax;
 
 
-		if (leftValue>leftMax) {
-			leftMax = leftValue;
-		}
-		float leftPercent = leftValue / leftMax;
+		//if (leftValue>leftMax) {
+		//	leftMax = leftValue;
+		//}
+		//float leftPercent = leftValue / leftMax;
 
 
-		float height = (rightPercent + leftPercent) / 2;	//Get current height (0 - 1, aka 0 - 100%)
-		float dotX = rightPercent / (rightPercent + leftPercent); 		//Get X value -> whether dot is closer to left, right, or middle (0 to 1)
+		float height = (rightValue + leftValue) / 2;	//Get current height (0 - 1, aka 0 - 100%)
+		float dotX = rightValue / (rightValue + leftValue); 		//Get X value -> whether dot is closer to left, right, or middle (0 to 1)
 
 		//adjust dot position
 		moveDot (height, dotX, dotWidth, dotHeight);
@@ -76,6 +79,8 @@ public class runtimeScript : MonoBehaviour {
 
 		//ADJUST TARGET
 		float targetPercentage = getTargetPercentage();
+		float targetWidth = getLRError() * 2;
+		float targetHeight = getAmplitudeError() * 2;
 		float mid = (float)0.5;
 		moveTarget (targetPercentage, mid, targetWidth, targetHeight);
 
@@ -109,7 +114,10 @@ public class runtimeScript : MonoBehaviour {
 		targetObject.rectTransform.localPosition = newTargetPosition;
 
 		//update target size
-		targetObject.rectTransform.sizeDelta = new Vector2(width, height);
+		float targetHeight = graphHeight * height;
+		float targetWidth = graphWidth * width;
+
+		targetObject.rectTransform.sizeDelta = new Vector2(targetWidth, targetHeight);
 	}
 
 
@@ -127,26 +135,26 @@ public class runtimeScript : MonoBehaviour {
 		device = SteamVR_Controller.Input ((int)trackedObjectLeft.index);
 		device.TriggerHapticPulse (900);
 		Debug.Log ("Left trigger pressed");
-		setMax();
+		//setMax();
 	}
 	void RightTriggerPressed(object sender, ClickedEventArgs e){
 		device = SteamVR_Controller.Input ((int)trackedObjectRight.index);
 		device.TriggerHapticPulse (900);
 		Debug.Log ("Right trigger pressed");
-		setMax ();
+		//setMax ();
 	}
 
 	float getLeftData(){
 		StreamReader sr = new StreamReader (pathToLeftData);
 		string leftDataString = sr.ReadToEnd ();
-		float leftValue = (float)double.Parse (leftDataString);
+		float leftValue = (float)double.Parse (leftDataString) / 100;
 		sr.Close ();
 		return leftValue;
 	}
 	float getRightData(){
 		StreamReader sr = new StreamReader (pathToRightData);
 		string rightDataString = sr.ReadToEnd ();
-		float rightValue = (float)double.Parse (rightDataString);
+		float rightValue = (float)double.Parse (rightDataString) / 100;
 		sr.Close ();
 		return rightValue;
 	}
@@ -156,9 +164,23 @@ public class runtimeScript : MonoBehaviour {
 		sr.Close ();
 		return (float)double.Parse (targetPercentageString) / 100;
 	}
-		
-	void setMax(){
-		leftMax = getLeftData ();
-		rightMax = getRightData ();
+	float getLRError()
+	{
+		StreamReader sr = new StreamReader(pathToLRErrorData);
+		string LRErrorString = sr.ReadToEnd();
+		sr.Close();
+		return (float)double.Parse(LRErrorString) / 100;
 	}
+	float getAmplitudeError()
+	{
+		StreamReader sr = new StreamReader(pathToAmplitudeErrorData);
+		string amplitudeErrorString = sr.ReadToEnd();
+		sr.Close();
+		return (float)double.Parse(amplitudeErrorString) / 100;
+	}
+		
+	//void setMax(){	No Longer Necessary as values are turned into percentages in LabView
+	//	leftMax = getLeftData ();
+	//	rightMax = getRightData ();
+	//}
 }
