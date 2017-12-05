245e669eede2f0bf8b3ef6c9bab417547545de7d
jakecronin <jake.cronin4@gmail.com>
2017-12-05 15:49:06 -0500

    small change to matlab file

--- LabviewFiles/Data/plotBiofeedbackData.m
+++ LabviewFiles/Data/plotBiofeedbackData.m
@@ -1,7 +1,12 @@
 clear all;
-a = csvread('Trial01_FilteredData.csv');
-filtData = a(any(a,2),:);
-x = filtData(:,1);
-y = filtData(:,2);
+a = csvread('DoubleRemoveWhitesFinishedDataFormatting/Trial02_FilteredData.csv');
+cleanData = a(any(a,2),:); %removes rows of all zero or NAN data
+x = cleanData(:,1);
+y = cleanData(:,2);
 
+subplot(2,1,1);
+plot(x)
+title("Make sure timestamps are continuous")
+subplot(2,1,2);
 plot(x,y);
+title("Data")
