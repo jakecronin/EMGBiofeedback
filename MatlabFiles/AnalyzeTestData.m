3d963a0de0743b2036d67581dc740b411355f8ce
jakecronin <jake.cronin4@gmail.com>
2018-03-23 13:21:45 -0400

    modified analyze data script

--- MatlabFiles/AnalyzeTestData.m
+++ MatlabFiles/AnalyzeTestData.m
@@ -9,31 +9,33 @@ close all
 % 18% trials 17 - 19
 % 20% trials 20 - 22
 
-trialNames = [5 10 15 20];%[5 8 10 12 15 18 20];
+trialNames = [5 8 10 12 15 20];
 %trialsOfInterest = %[1 4 8 11 14 17 20];
 channel = 1;
 colors = ['rgbmkyc'];
 
-filteredPattern = '*_FilteredData.csv';
-displayPattern = '*_DisplayData.csv';
+dirPath = '../LabviewFiles/Data/Default/';
 
-filtfiles = dir(filteredPattern);
-dispfiles = dir(displayPattern);
-size(trialsOfInterest,2)
+filtfiles = dir(strcat(dirPath,'*_FilteredData.csv'));
+dispfiles = dir(strcat(dirPath,'*_DisplayData.csv'));
+size(trialNames,2)
 
 pd = gobjects(size(trialNames,1),1); %clear plot array
+dispPlot = gobjects(size(trialNames,1),1);
 
 stdevs = zeros(size(trialNames,2),1);
 means = zeros(size(trialNames,2),1);
 
+
+%Get trial numbers for 
 
 for i = size(trialNames,2):-1:1
 
     %load trial data
     trialNum = Log(Log.TrialName == trialNames(i),'TrialNumber');
     trialNum = trialNum.TrialNumber(1);
-    filtered = csvread(filtfiles(trialsOfInterest(1,i),1).name,1,0);
-    display = csvread(dispfiles(trialsOfInterest(1,i),1).name,1,0);
+    filtered = csvread(strcat(dirPath,filtfiles(trialNum).name),1,0);
+    display = csvread(strcat(dirPath,dispfiles(trialNum).name),1,0);
     
     %extract only time stamps and display channel
     filt = filtered(:,1:2);
@@ -52,11 +54,11 @@ for i = size(trialNames,2):-1:1
     figure(1)
     pf(i) = plot(filt(:,1), filt(:,2), colors(i));
     hold on;
-    pd(i) = plot(disp(:,1), disp(:,2), colors(i),'LineWidth',2);
+    pd(i) = plot(disp(:,1), disp(:,2), colors(i),'LineWidth',4);
     
     %Get stdev of filtered data
-    stddev(i,1) = std(filt(1000:9000,2));
-    means(i,1) = mean(filt(1000:9000,2));
+    stddev(i,1) = std(filt(:,2)); %std(filt(1000:9000,2));
+    means(i,1) = mean(filt(:,2)); %mean(filt(1000:9000,2));
     
     figure(2);hold on;
     peaks = findpeaks(filt(:,2), 'MinPeakWidth', 5);
@@ -64,6 +66,10 @@ for i = size(trialNames,2):-1:1
     
     figure(3); hold on;
     histfit(peaks);
+    
+    figure(4); hold on;
+    dispPlot(i) = plot(disp(:,1), disp(:,2), colors(i),'LineWidth',4);
+
 end
 
 figure(1)
@@ -81,6 +87,14 @@ xlim([0 inf]);
 ylim([0 100]);
 
 
+figure(2)
+title('Filtered Data Peaks');
+
+figure(3)
+title('Histogram of filtered data peaks');
+
+figure(4)
+title('Plot of Display Values');
 
 %try building histfit and normal dist plot
 
